844be4432d09a13999bb88f60eb994c0aea36164
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-09-07 18:56:37 +0000

    add canvas

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
-$(document).ready(function() {
+var DunCrawl = DunCrawl || {};
 
-  // Place JavaScript code here...
+DunCrawl.game = new Phaser.Game(360, 640, Phaser.AUTO);
 
-});
+DunCrawl.game.state.add('Boot', DunCrawl.BootState);
+DunCrawl.game.state.add('Preload', DunCrawl.PreloadState);
+DunCrawl.game.state.add('Game', DunCrawl.GameState);
+
+DunCrawl.game.state.start('Boot');
