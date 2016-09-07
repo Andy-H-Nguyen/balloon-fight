beaaa1a4788389e2a3deb0b5e7be97e545f8a2f6
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-09-07 22:08:15 +0000

    player physics

--- public/js/states/Game.js
+++ public/js/states/Game.js
@@ -8,13 +8,13 @@ Achicken.GameState = {
     this.MAX_SPEED_SHOOT = 1100;
     this.SHOOT_FACTOR = 12;
     this.KILL_DIFF = 25;
-
+    
     //keep track of the current level
     this.currentLevel = currentLevel ? currentLevel : 'level1';
-
+    
     //gravity
     this.game.physics.p2.gravity.y = 1000;
-
+    
     //collision groups - if you want to specify what collides with what
     this.blocksCollisionGroup = this.game.physics.p2.createCollisionGroup();
     this.enemiesCollisionGroup = this.game.physics.p2.createCollisionGroup();
@@ -26,34 +26,33 @@ Achicken.GameState = {
     //sky background
     this.sky = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'sky');
     this.game.world.sendToBack(this.sky);
-
+    
     this.chickenHUD = this.add.group();
-
+    
     //enemies
     this.enemies = this.add.group();
     this.enemies.enableBody = true;
     this.enemies.physicsBodyType = Phaser.Physics.P2JS;
-
+    
     //blocks
     this.blocks = this.add.group();
     this.blocks.enableBody = true;
     this.blocks.physicsBodyType = Phaser.Physics.P2JS;
-
+    
     //bodies in p2 -> they get their anchor points set to 0.5
     this.floor = this.add.tileSprite(this.game.world.width/2, this.game.world.height - 24, this.game.world.width, 48, 'floor');
     this.blocks.add(this.floor);
-
+    
     //specify collision for the floor
     this.floor.body.setCollisionGroup(this.blocksCollisionGroup);
     this.floor.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup, this.chickensCollisionGroup]);
     this.floor.body.static = true;
-
-	this.player = this.add.group();
-	this.player.enableBody = true;
-	this.player.physicsBodyType = Phaser.Physics.P2JS;
-	this.player.sprite = this.add.sprite(180, 400, 'chicken');
-	this.game.physics.p2.enable(this.player);
-  },
+    
+    this.player = this.add.sprite(180, 400, 'chicken');
+    this.player.enableBody = true;
+    this.player.physicsBodyType = Phaser.Physics.P2JS;
+    this.game.physics.p2.enable(this.player);
+},
 
   update: function() {  
 
