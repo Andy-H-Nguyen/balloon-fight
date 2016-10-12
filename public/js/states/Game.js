3714c093106d312df4811fb1c259914de26e5ac4
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-10-12 02:31:58 +0000

    get server keys

--- public/js/states/Game.js
+++ public/js/states/Game.js
@@ -1,5 +1,6 @@
 var Achicken = Achicken || {};
-var socket = io.connect(window.location.href);;
+var socket = io();
+
 socket.on('greet', function(data) {
 	console.log(data);
 	socket.emit('respond', {message: 'Hello to you too, server'});
@@ -57,13 +58,16 @@ Achicken.GameState = {
 
 	update: function() {
 		if (this.cursors.left.isDown) {
-			this.player.body.applyForce([10, 0], this.player.x, this.player.y);
+			socket.emit('keydown', Phaser.KeyCode.LEFT);
+			//this.player.body.applyForce([10, 0], this.player.x, this.player.y);
 		}
 		if (this.cursors.right.isDown) {
-			this.player.body.applyForce([-10, 0], this.player.x, this.player.y);
+			socket.emit('keydown', Phaser.KeyCode.RIGHT);
+			//this.player.body.applyForce([-10, 0], this.player.x, this.player.y);
 		}
 		if (this.cursors.up.upDuration()) {
-			this.player.body.moveUp(200);
+			socket.emit('keyup', Phaser.KeyCode.UP);
+			//this.player.body.moveUp(200);
 		}
 	}
 };
--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-var express = require('express');
+var PORT = process.env.PORT;
+var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/about', function(req, res) {
     res.send('About us');  
 });
-
-app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Starting server at " + process.env.IP + " on port " + process.env.PORT);
 });
 
 io.on('connection', (socket) => {
+  // Test code so we so Socket.io worked
   socket.emit('greet', { hello: 'Hey there browser!' });
-  socket.on('respond', (data) => {
-    console.log(data);
+  
+  socket.on("keydown", function(key){
+      console.log("keyup: " + key + " from " + socket._id);
   });
+    
+  socket.on("keyup", function(key){
+      console.log("keyup: " + key + " from " + socket._id);
+  });
+  
   socket.on('disconnect', () => {
     console.log('Socket disconnected');
   });
